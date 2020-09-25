4fef31cb93118058790056d2eb058ba3bcbae239
DylanAye <dylaaye@gmail.com>
2020-09-25 12:16:54 +0200

    Début Bootstrap fil rouge

--- Fil rouge/HTML/header.php
+++ Fil rouge/HTML/header.php
@@ -1,16 +1,45 @@
 <html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+        <link rel="stylesheet" href="styles.css">
+    </head>
     <body>
-        <div class="container-fluid">
-            d
+        <div class="container">          
+            <div class="row">
+                <div class='col-lg-4'>
+                    <img src="img/logo_village_green.png">
+                </div>
+                <div class='row'>
+                    <div class="col-md-5">
+                        Infos
+                    </div>
+                </div>
+                <div class='row'>
+                
 
-
-
-
-
-
-
-
+                    <div class="col-md-1">
+                        Produits
+                    </div>
+                    <div class="col-md-1">
+                        Services
+                    </div>
+                    <div class="col-md-1">
+                        Aide
+                    </div>
+                    <div class="col-md-1">
+                        À propos
+                    </div>
+                </div>
+            </div>
         </div>
+
+
+
 
 
     </body>
