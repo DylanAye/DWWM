29988a3c4536f46e9a7b0f83fc684d91aad83e6b
DylanAye <dylaaye@gmail.com>
2020-10-02 12:28:23 +0200

    Suite header

--- Fil rouge/HTML/header.php
+++ Fil rouge/HTML/header.php
@@ -6,35 +6,42 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-        <link rel="stylesheet" href="styles.css">
+        <link rel="stylesheet" href="styles/styles.css">
     </head>
     <body>
         <div class="container">          
-            <div class="row">
-                <div class='col-lg-4'>
-                    <img src="img/logo_village_green.png">
-                </div>
-                <div class='row'>
-                    <div class="col-md-5">
-                        Infos
-                    </div>
-                </div>
-                <div class='row'>
-                
+            <div class='col'  id='multicouche'>
+                <img src="img/logo_village_green.png" id='logo'>
+                <img src='img/3_bandes.png' id='bandes'>
+            </div>
 
-                    <div class="col-md-1">
-                        Produits
-                    </div>
-                    <div class="col-md-1">
-                        Services
-                    </div>
-                    <div class="col-md-1">
-                        Aide
-                    </div>
-                    <div class="col-md-1">
-                        À propos
-                    </div>
-                </div>
+            <div class = 'row' id='ligne1'>
+                <div class='col-7'></div>
+                <div class='col-1'>Infos</div>
+                <div class='col-2'>Espace client</div>
+                <div class='col-1'><img src='img/picto_panier.png'></div>
+                <div class='col-1'><img src='img/picto_pays.png'></div>
+            </div>
+
+            <div class='row' id='ligne2'>
+                <div class='col-4'></div>
+                <div class="col text-center">Produits</div>
+                <div class="col text-center">Services</div>
+                <div class="col text-center">Aide</div>
+                <div class="col text-center">À propos</div>
+            </div>
+
+            <div class='row' id='ligne3'>
+                <div class='col-4'></div>
+                <div class="col text-center">Guit/Bass</div>
+                <div class="col text-center">Batteries</div>
+                <div class="col text-center">Clavier</div>
+                <div class="col text-center">Studio</div>
+                <div class="col text-center">Sono</div>
+                <div class="col text-center">Éclairage</div>
+                <div class="col text-center">DJ</div>
+                <div class="col text-center">Cases</div>
+                <div class="col text-center">Accessoires</div>
             </div>
         </div>
 
